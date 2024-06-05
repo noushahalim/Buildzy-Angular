91e6f947afdf5ed8b0fa893c1b0da67b1405ffcb
noushahalim <noushahalim@gmail.com>
2024-06-05 14:58:12 +0530

    connected signup forms with backend and used response for next process

--- src/app/modules/auth/clientSignup/clientSignup.component.ts
+++ src/app/modules/auth/clientSignup/clientSignup.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
+import { Router } from "@angular/router";
+import { signupModel } from "src/app/models/signup";
+import { ClientService } from "src/app/services/client.service";
 
 @Component({
     selector:'auth-clientSignup',
@@ -6,5 +9,20 @@ import { Component } from "@angular/core";
 })
 
 export class ClientSignupComponent{
-    
+
+    constructor(private clientService:ClientService, private route: Router){}
+
+    @Output() formDataReceived = new EventEmitter<any>();
+
+    onFormDataSubmitted(formData: any) {
+        const datas:signupModel =formData as signupModel
+        this.clientService.clientSignup(datas).subscribe(
+            (response)=>{
+                this.route.navigate(['/auth/login'])
+            },
+            (error:any)=>{
+                console.log(error)
+            }
+        )
+    }
 }
--- src/app/modules/auth/engineerSignup/engineerSignup.component.ts
+++ src/app/modules/auth/engineerSignup/engineerSignup.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
+import { Router } from "@angular/router";
+import { signupModel } from "src/app/models/signup";
+import { EngineerService } from "src/app/services/engineer.service";
 
 @Component({
     selector:'auth-engineerSignup',
@@ -6,5 +9,19 @@ import { Component } from "@angular/core";
 })
 
 export class EngineerSignup{
-    
+    constructor(private engineerService:EngineerService, private route: Router){}
+
+    @Output() formDataReceived = new EventEmitter<any>();
+
+    onFormDataSubmitted(formData: any) {
+        const datas:signupModel =formData as signupModel
+        this.engineerService.engineerSignup(datas).subscribe(
+            (response)=>{
+                this.route.navigate(['/auth/login'])
+            },
+            (error:any)=>{
+                console.log(error)
+            }
+        )
+    }
 }
